4287afbf5a49c26fc1985da432ddf437863f91d4
LordA2117 <vip20dpa@gmail.com>
2025-04-27 19:09:49 +0530

    Added particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
+import { useState, useEffect, useMemo } from 'react';
 import './App.css'
 import QuizCard from './components/QuizCard';
 import CorrectCard from './components/CorrectCard';
 import WrongCard from './components/WrongCard';
 import LoginCard from './components/LoginCard';
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import particlesConfig from './components/particlesConfig';
 
 function App() {
+	const [userScore, setUserScore] = useState(0);
+	const [totalScore, setTotalScore] = useState(0); // Total score of all the questions so far
+	const [index, setIndex] = useState(0); // Question number
+	const [answerState, setAnswerState] = useState(false); // Answer is wrong or right (false or true)
+	const [init, setInit] = useState(false);
+
+	useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+	const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+	const options = useMemo(
+		() => (particlesConfig));	
 
   return (
     <div className='App'>
-			<LoginCard />
+			<Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+			<section className='content'>
+				<LoginCard />
+			</section>
     </div>
   )
 }
@@ -16,4 +50,4 @@ function App() {
 export default App;
 
 // Used lottiefiles for animations, very cool shi
-// TODO: Build a simple introduction card and a simple name and scoring system. Style mouse pointer and add a circular progress bar around it.
+// Implement the final scorecard, finish the app and also see if there are any better particle configs that will suit this, because this seems a little tacky.
---
+++ src/components/particlesConfig.js
@@ -0,0 +1,68 @@
+const particlesConfig = {
+  "fps_limit": 120,
+  "interactivity": {
+    "detectsOn": "canvas",
+    "events": {
+      "onClick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "onHover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 4
+      },
+      "repulse": {
+        "distance": 100,
+        "duration": 0.4
+      }
+    }
+  },
+  "particles": {
+    "color": {
+      "value": "#888888" // grey color
+    },
+    "links": {
+      "color": "#888888", // match links to particles
+      "distance": 150,
+      "enable": true,
+      "opacity": 0.5, // make the links more transparent
+      "width": 1
+    },
+    "move": {
+      "bounce": false,
+      "direction": "none",
+      "enable": true,
+      "outMode": "out",
+      "random": true,
+      "speed": 2,
+      "straight": false
+    },
+    "number": {
+      "density": {
+        "enable": true,
+        "area": 800
+      },
+      "value": 100
+    },
+    "opacity": {
+      "value": 0.5 // make the particles semi-transparent
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "random": true,
+      "value": 3
+    }
+  },
+  "detectRetina": true
+};
+
+export default particlesConfig;
+
